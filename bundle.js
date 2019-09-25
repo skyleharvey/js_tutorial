486ae526cc97bb65108925d888c8e34b9ed34bca
Kyle Harvey <kyleharv@gmail.com>
2019-09-25 18:03:43 -0400

    add palindrome test button to palindrome.html and functionality for the button via alerts in main.js

--- bundle.js
+++ bundle.js
@@ -1,14 +1,23 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const Phrase = require('kyleharv-Palindrome');
 
-let testString = prompt("Please enter a string to test for palindrome-ness!");
-let testPhrase = new Phrase(testString);
+function palindromeTester() {
+  let testString = prompt("Please enter a string to test for palindrome-ness!");
+  let testPhrase = new Phrase(testString);
 
-if (testPhrase.palindrome()) {
-  alert(`"${testPhrase.content}" is a palindrome!`);
-} else {
-  alert(`"${testPhrase.content}" is not a palindrome.`);
+  if (testPhrase.palindrome()) {
+    alert(`"${testPhrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${testPhrase.content}" is not a palindrome.`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let palindromeTestThingy = document.querySelector("#palindromeTestButton");
+  palindromeTestThingy.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
 
 },{"kyleharv-Palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const Phrase = require('kyleharv-Palindrome');
 
-let testString = prompt("Please enter a string to test for palindrome-ness!");
-let testPhrase = new Phrase(testString);
+function palindromeTester() {
+  let testString = prompt("Please enter a string to test for palindrome-ness!");
+  let testPhrase = new Phrase(testString);
 
-if (testPhrase.palindrome()) {
-  alert(`"${testPhrase.content}" is a palindrome!`);
-} else {
-  alert(`"${testPhrase.content}" is not a palindrome.`);
+  if (testPhrase.palindrome()) {
+    alert(`"${testPhrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${testPhrase.content}" is not a palindrome.`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let palindromeTestThingy = document.querySelector("#palindromeTestButton");
+  palindromeTestThingy.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
